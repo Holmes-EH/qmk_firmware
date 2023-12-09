8eb241aa7019de014b9bf13dc8730a7358805723
Samuel Holmes <samuel.holmes@origins-digital.com>
2023-12-09 08:29:58 +0100

    change keycode case order

--- keyboards/keychron/v10/iso_encoder/keymaps/Holmes-EH/keymap.c
+++ keyboards/keychron/v10/iso_encoder/keymaps/Holmes-EH/keymap.c
@@ -25,16 +25,17 @@ enum custom_keycodes {
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
-        case H_MAC1:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_LALT(SS_LCTL(SS_TAP(X_B)))));
-            }
-        return false;
         case H_MAC5:
             if (record->event.pressed) {
                 SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_Q))));
             }
-        return false;}
+            return false;
+        case H_MAC1:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI(SS_LALT(SS_LCTL(SS_TAP(X_B)))));
+            }
+            return false;
+    }
     return true;
 }
 
